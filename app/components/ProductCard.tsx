f1121fe3b9678a9e0c4513a14f07169c5b759834
ml567 <miyunilowe5@gmail.com>
2026-05-16 20:19:09 +0530

    Create ProductCard.tsx

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button onClick={() => console.log('Button clicked!')}>Click me</button>
+    </div>
+  )
+}
+
+export default ProductCard
